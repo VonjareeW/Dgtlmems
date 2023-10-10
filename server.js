eaed53d9fd740bbcf6726a1e24d51c1fd844c8ae
VonjareeW <52430595+VonjareeW@users.noreply.github.com>
2023-10-10 17:32:32 -0400

    added middleware

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const PORT = process.env.PORT || 3001;
 //  set the port
 const app = express();
 
+// middleware will run in correlation 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(htmlRoutes)
+app.use(apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
